9d59d7007988cc7ae41da55d97484c2f801a5689
Feiteng <fl750@nyu.edu>
2017-05-26 08:08:11 -0400

    update

--- src/milk2.java
+++ src/milk2.java
@@ -12,11 +12,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class milk2
@@ -25,7 +21,7 @@ public class milk2
 	static BufferedReader f;
 	static PrintWriter out;
 	static String filename = "milk2";
-	static boolean test = true;
+	static boolean test = false;
 
 	public static void main( String[] args ) throws IOException
 	{
@@ -54,87 +50,34 @@ public class milk2
 		StringTokenizer st;
 		st = new StringTokenizer( f.readLine() );
 		int n = Integer.valueOf( st.nextToken() );
-		int[][] times = new int[n][2];
+		int[] start = new int[n], end = new int[n];
 		for ( int i = 0; i < n; i++ )
 		{
 			st = new StringTokenizer( f.readLine() );
-			times[i][0] = Integer.valueOf( st.nextToken() );
-			times[i][1] = Integer.valueOf( st.nextToken() );
+			start[i] = Integer.valueOf( st.nextToken() );
+			end[i] = Integer.valueOf( st.nextToken() );
 		}
 
-		int[] v = val( times );
-		out.printf( "%d %d\n", v[0], v[1] );
-	}
+		Arrays.sort( start );
+		Arrays.sort( end );
 
-	int[] val( int[][] times )
-	{
-		List<Integer[]> list = new ArrayList<>();
-		int[] ret = new int[2];
-		for ( int[] t : times )
-			list.add( new Integer[] { t[0], t[1] } );
-		Collections.sort( list, ( a, b ) -> ( a[1] == b[1] ? a[0] - b[0] : ( b[1] - b[0] ) - ( a[1] - a[0] ) ) );
-		Map<Integer, Integer> map = new HashMap<>();
-		map.put( list.get( 0 )[0], list.get( 0 )[1] );
-		for ( Integer[] v : list )
-			updateMap( v[0], v[1], map );
-		//
-		List<Integer> list2 = new ArrayList<>( map.keySet() );
-		Collections.sort( list2 );
-		ret[0] = map.get( list2.get( 0 ) ) - list2.get( 0 );
-		// ret[1] = list2.get( 1 ) - map.get( list2.get( 0 ) );
-		for ( int k = 1; k < list2.size(); k++ )
+		int i = 0, j = 0, maxLen = 0, maxGap = 0;
+		while ( i < start.length - 1 )
 		{
-			ret[0] = Math.max( map.get( list2.get( k ) ) - list2.get( k ), ret[0] );
-			ret[1] = Math.max( ret[1], list2.get( k ) - map.get( list2.get( k - 1 ) ) );
+			while ( i < start.length - 1 && end[i] >= start[i + 1] )
+				i++;
+			// now either at end, or found next start
+			maxLen = Math.max( maxLen, end[i] - start[j] );
+			if ( i < start.length - 1 )
+				maxGap = Math.max( maxGap, start[i + 1] - end[i] );
+			j = i + 1;
+			i++;
 		}
-		if ( test )
-			System.out.println( map );
-		return ret;
+		// if at end
+		if ( i < start.length )
+			maxLen = Math.max( maxLen, end[i] - start[j] );
 
-	}
-
-	void updateMap( int v0, int v1, Map<Integer, Integer> map )
-	{
-		Map<Integer, Integer> mapCopy = new HashMap<>( map );
-		for ( int k : map.keySet() )
-		{
-			int k0 = k, k1 = map.get( k0 );
-			if ( k0 == v0 && k1 == v1 )
-				continue;
-			if ( k0 <= v0 && k1 >= v1 ) // inclusion
-				continue;
-			if ( k0 >= v0 && k1 <= v1 ) // bigger interval
-			{
-				mapCopy.put( v0, v1 );
-				mapCopy.remove( k0 );
-				// itor.remove();
-			}
-			if ( k1 < v0 || k0 > v1 ) // no intersection
-				mapCopy.put( v0, v1 );
-			else // partial
-			{
-				if ( k0 == Math.min( k0, v0 ) )
-					mapCopy.put( k0, Math.max( k1, v1 ) );
-				else
-				{
-					mapCopy.remove( k0 );
-					// itor.remove();
-					mapCopy.put( v0, Math.max( k1, v1 ) );
-					v1 = Math.max( k1, v1 );
-				}
-			}
-
-		}
-		map.putAll( mapCopy );
-
-	}
-
-	int sum( Iterable<Integer> list )
-	{
-		int sum = 0;
-		for ( int k : list )
-			sum += k;
-		return sum;
+		out.printf( "%d %d\n", maxLen, maxGap );
 	}
 
 }
