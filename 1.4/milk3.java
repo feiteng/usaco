7bbb682c945dead725996047411310a82798c76e
Feiteng <fl750@nyu.edu>
2017-06-07 09:02:44 -0400

    1.5

--- 1.4/milk3.java
+++ 1.4/milk3.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -23,7 +24,7 @@ public class milk3
 
 	static String filename = "milk3";
 
-	static boolean test = true;
+	static boolean test = false;
 
 	static void run() throws IOException
 	{
@@ -32,8 +33,8 @@ public class milk3
 		int a, b, c;
 		if ( test )
 		{
-			a = 8;
-			b = 9;
+			a = 2;
+			b = 5;
 			c = 10;
 		}
 		else
@@ -49,9 +50,14 @@ public class milk3
 		visited = new boolean[c1 + 1][c2 + 1][c3 + 1];
 		dfs( 0, 0, c );
 		String string = "";
-		List<String> sList = new ArrayList<>();
+		List<Integer> iList = new ArrayList<>();
 		for ( int k : set )
+			iList.add( k );
+		Collections.sort( iList );
+		List<String> sList = new ArrayList<>();
+		for ( int k : iList )
 			sList.add( String.valueOf( k ) );
+
 		string = String.join( " ", sList );
 		// string += "\n";
 		out.println( string );
@@ -63,36 +69,81 @@ public class milk3
 
 	static void dfs( int a, int b, int c )
 	{
-		if ( visited[a][b][c] )
+		if ( visited[a][b][c] || a < 0 || b < 0 || c < 0 )
 			return;
 		visited[a][b][c] = true;
 		if ( a == 0 ) // add c
-		{
 			set.add( c );
+		int from, to, toCap, tofill, pourout;
+		if ( a != 0 )
+		{ // pour a into b and c
+			from = a;
+			if ( b != c2 )
+			{
+				toCap = c2;
+				to = b;
+				tofill = toCap - to < from ? toCap - to : from;
+				pourout = from - tofill;
+				dfs( a - tofill, b + tofill, c );
+			}
+			if ( c != c3 )
+			{
+				toCap = c3;
+				to = c;
 
-			// c -> a
-			if ( c1 <= c )
-				dfs( c1, b, c - c1 );
-			else
-				dfs( c, b, 0 );
-			// c -> b
-			if ( b + c <= c2 )
-				dfs( a, b + c, 0 );
-			else
-				// b + c > c2 -> c > c2-b
-				dfs( a, c2, c - ( c2 - b ) );
+				tofill = toCap - to < from ? toCap - to : from;
+				pourout = from - tofill;
+
+				dfs( a - tofill, b, c + tofill );
+			}
+
 		}
-		else // a not zero, pour a into b and c
-		{
-			if ( b + a <= c2 )
-				dfs( 0, b + a, c );
-			else // b + a > c2 -> a > c2 - b
-				dfs( a - ( c2 - b ), c2, c );
-			if ( c + a <= c3 )
-				dfs( 0, b, c + a );
-			else
-				dfs( a - ( c3 - c ), b, c3 );
+		if ( b != 0 )
+		{// pour b into a and c
+			from = b;
+			if ( a != c1 )
+			{
+				toCap = c1;
+				to = a;
+				tofill = toCap - to < from ? toCap - to : from;
+				pourout = from - tofill;
+				dfs( a + tofill, b - tofill, c );
+			}
+			if ( c != c3 )
+			{
+				toCap = c3;
+				to = c;
+
+				tofill = toCap - to < from ? toCap - to : from;
+				pourout = from - tofill;
+
+				dfs( a, b - tofill, c + tofill );
+			}
+
 		}
+		if ( c != 0 )
+		{// pour c into a and b
+			from = c;
+			if ( a != c1 )
+			{
+				toCap = c1;
+				to = a;
+				tofill = toCap - to < from ? toCap - to : from;
+				pourout = from - tofill;
+				dfs( a + tofill, b, c - tofill );
+			}
+			if ( b != c2 )
+			{
+				toCap = c2;
+				to = b;
+
+				tofill = toCap - to < from ? toCap - to : from;
+				pourout = from - tofill;
+
+				dfs( a, b + tofill, c - tofill );
+			}
+		}
+
 	}
 
 	static BufferedReader reader;
---
+++ 1.5/numtri.java
@@ -0,0 +1,86 @@
+
+/*-
+ ID: feiteng li
+ LANG: JAVA
+ TASK: numtri
+ */
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class numtri
+{
+
+	static String filename = "numtri";
+
+	static boolean test = true;
+
+	static void run() throws IOException
+	{
+		StringTokenizer st;
+		st = new StringTokenizer( reader.readLine() );
+		int n = Integer.valueOf( st.nextToken() ), maxVal = 0;
+		int[] a = new int[n], b = new int[n];
+		for ( int i = 1; i < n + 1; i++ ) // outer loop
+		{
+			st = new StringTokenizer( reader.readLine() );
+			for ( int j = 0; j < i; j++ ) // array calculation
+			{
+				int k = Integer.valueOf( st.nextToken() );
+				if ( j == 0 )
+					a[j] += k;
+				else if ( j == i - 1 )
+					a[j] = a[j - 1] + k;
+				else
+					a[j] = Math.max( a[j - 1], a[j] ) + k;
+
+				maxVal = Math.max( maxVal, a[j] );
+			}
+			System.out.println( Arrays.toString( a ) );
+
+		}
+
+		out.println( maxVal );
+	}
+
+	public static void main( String[] args ) throws IOException
+	{
+
+		long t = System.currentTimeMillis();
+		setup();
+		run();
+
+		if ( test )
+			System.out.printf( "Run time... %s ms\n", System.currentTimeMillis() - t );
+
+		out.close(); // close the output file
+
+	}
+
+	static void setup() throws IOException
+	{
+
+		if ( test )
+		{
+			reader = new BufferedReader( new InputStreamReader( System.in ) );
+			out = new PrintWriter( System.out );
+		}
+		else
+		{
+			reader = new BufferedReader( new FileReader( filename + ".in" ) );
+			out = new PrintWriter( new BufferedWriter( new FileWriter( filename + ".out" ) ) );
+		}
+
+	}
+
+	static BufferedReader reader;
+	static PrintWriter out;
+
+}
