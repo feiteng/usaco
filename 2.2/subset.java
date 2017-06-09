435cd2fee3f571b4c8a6e05c2531d4f4d2df6f09
Feiteng <li.feiteng@gmail.com>
2017-06-09 13:04:02 -0400

    subset

--- 2.2/subset.java
+++ 2.2/subset.java
@@ -12,10 +12,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class subset
@@ -30,84 +26,25 @@ public class subset
 
 		StringTokenizer st;
 		st = new StringTokenizer( reader.readLine() );
-		int n = Integer.valueOf( st.nextToken() );
-		Map<Integer, Long> map = new HashMap<>();
-		map.put( 3, 1l );
-		map.put( 4, 1l );
-		map.put( 7, 4l );
-		map.put( 8, 7l );
-		map.put( 11, 35l );
-		map.put( 12, 62l );
-		map.put( 15, 361l );
-		map.put( 16, 657l );
-		map.put( 20, 7636l );
-		map.put( 24, 93846l );
-		map.put( 27, 632602l );
-		map.put( 28, 1199892l );
-		map.put( 31, 8273610L );
-		map.put( 32, 15796439l );
-		map.put( 35, 110826888l );
-		map.put( 36, 212681976l );
-		map.put( 39, 1512776590L );
-		if ( map.containsKey( n ) )
-			out.println( map.get( n ) );
-		else
+		int n = Integer.valueOf( st.nextToken() ), sum = ( n + 1 ) * n / 2, runSum = 1, k = 0;
+		if ( ( sum & 1 ) != 0 )
+		{
 			out.println( 0 );
-		// int[] nums = new int[n];
-		// Set<Integer> set = new HashSet<>();
-		// for ( int i = 1; i <= n; i++ )
-		// {
-		// nums[i - 1] = i;
-		// set.add( i );
-		// }
-		// generateCombs( nums, 0, 0, sumList( set ) );
-		// out.println( subsetCount / 2 );
-	}
-
-	long subsetCount = 0;
-
-	int sumList( Iterable<Integer> list )
-	{
-		int k = 0;
-		for ( int m : list )
-			k += m;
-		return k;
-
-	}
-
-	void generateCombs( int[] pos, int currentPos, int sum1, int sum2 )
-	{
-		if ( sum1 == sum2 )
-		{
-			// System.out.println( combs );
-			subsetCount++;
 			return;
 		}
-		if ( currentPos >= pos.length )
-			return;
-		for ( int i = currentPos; i < pos.length; i++ )
-		{
-			sum1 += pos[i];
-			sum2 -= pos[i];
-			generateCombs( pos, i + 1, sum1, sum2 );
-			sum1 -= pos[i];
-			sum2 += pos[i];
-		}
-
-	}
-
-	void run( int n )
-	{
-		// int n = Integer.valueOf( st.nextToken() );
-		int[] nums = new int[n];
-		Set<Integer> set = new HashSet<>();
+		long[] vals = new long[sum + 1];
+		vals[0] = 1;
 		for ( int i = 1; i <= n; i++ )
 		{
-			nums[i - 1] = i;
-			set.add( i );
+			runSum += k;
+			// System.out.printf( "%d %d \n", i, runSum );
+			for ( int j = runSum - 1; j >= 0; j-- )
+				vals[i + j] += vals[j];
+			// System.out.println( Arrays.toString( vals ) );
+			k++;
 		}
-		generateCombs( nums, 0, 0, sumList( set ) );
-		System.out.printf( "%d %d\n", n, subsetCount / 2 );
+
+		out.println( vals[sum / 2] / 2 );
 	}
 
 	public static void main( String[] args ) throws IOException
